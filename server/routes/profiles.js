835408ea49fdc1f1854894d63f696fe95f0ee044
Austin <hnitsua13@gmail.com>
2024-09-12 09:42:30 -0700

    added other routes

--- server/routes/profiles.js
+++ server/routes/profiles.js
@@ -10,19 +10,69 @@ router.get("/", (_, res) => {
 });
 
 router.post("/", (req, res) => {
-  res.json("post works");
+  const newProfile = new Profile({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    phone: req.body.phone,
+    location: req.body.location,
+    about: req.body.about,
+    projects: req.body.projects,
+    work: req.body.work,
+    skills: req.body.skills,
+    education: req.body.education,
+  });
+
+  newProfile
+    .save()
+    .then((profile) => res.json(profile))
+    .catch((err) => res.status(404));
 });
 
 router.put("/", (req, res) => {
-  res.json("put works");
+  Profile.findByIdAndUpdate.findOneAndUpdate(
+    { _id: req.body._id },
+    {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      phone: req.body.phone,
+      location: req.body.location,
+      about: req.body.about,
+      projects: req.body.projects,
+      work: req.body.work,
+      skills: req.body.skills,
+      education: req.body.education,
+    },
+    { new: true }
+  );
 });
 
-router.patch("/", (req, res) => {
-  res.json("patch works");
-});
+// router.patch("/", (req, res) => {
+//   Profile.findByIdAndUpdate.findOneAndUpdate(
+//     { _id: req.body._id },
+//     {
+//       $set: {
+//         firstName: req.body.firstName,
+//         lastName: req.body.lastName,
+//         email: req.body.email,
+//         phone: req.body.phone,
+//         location: req.body.location,
+//         about: req.body.about,
+//         projects: req.body.projects,
+//         work: req.body.work,
+//         skills: req.body.skills,
+//         education: req.body.education,
+//       },
+//     },
+//     { new: true }
+//   );
+// });
 
 router.delete("/:profileId", (req, res) => {
-  res.json(`Profile ${req.params.profileId} deleted`);
+  Profile.findByIdAndDelete(req.params.profileId)
+    .then((profile) => res.json(profile))
+    .catch((err) => res.status(404));
 });
 
 export const profilesRouter = router;
